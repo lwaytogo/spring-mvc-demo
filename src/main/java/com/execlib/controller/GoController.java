a214b240db3350105badfd9d62c204140b56f318
lidaoyuan <lidaoyuan@aliyun.com>
2025-09-11 22:30:29 +0800

    init

---
+++ src/main/java/com/execlib/controller/GoController.java
@@ -0,0 +1,26 @@
+package com.execlib.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GoController {
+    private static final Log logger = LogFactory.getLog(GoController.class);
+
+    @RequestMapping(value = {"/"}, method = {RequestMethod.HEAD})
+    public String head() {
+        return "go.jsp";
+    }
+
+    @RequestMapping(value = {"/index", "/"}, method = {RequestMethod.GET})
+    public String index(Model model) throws Exception{
+        logger.info("=================processed by index===================");
+        model.addAttribute("msg", "Go Go Go!");
+        return "go.jsp";
+    }
+
+}
---
+++ src/main/webapp/go.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false" %>
+<html>
+<head>
+    <title>let'sGo</title>
+</head>
+<body>
+    ${msg}
+</body>
+</html>
